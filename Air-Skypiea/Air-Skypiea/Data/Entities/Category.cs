087b32a224f090e0ed3480753bd5056201507ed4
DEI ROMERO <DEI ROMERO@DESKTOP-UNSED2L>
2022-05-17 21:53:28 -0500

    Finish Crud Category

--- Air-Skypiea/Air-Skypiea/Data/Entities/Category.cs
+++ Air-Skypiea/Air-Skypiea/Data/Entities/Category.cs
@@ -2,15 +2,13 @@
 
 namespace Air_Skypiea.Data.Entities
 {
-    public class Country
+    public class Category
     {
         public int Id { get; set; }
 
-        [Display(Name = "País")]
+        [Display(Name = "Categoria")]
         [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Name { get; set; }
-
-
     }
 }
