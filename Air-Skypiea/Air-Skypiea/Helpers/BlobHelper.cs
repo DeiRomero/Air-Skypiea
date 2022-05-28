8b63f12f5a4a21ba26e28141cc75ff39d926b449
DEI ROMERO <DEI ROMERO@DESKTOP-UNSED2L>
2022-05-27 21:04:41 -0500

    Initial Azure/UserRol

---
+++ Air-Skypiea/Air-Skypiea/Helpers/BlobHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Air_Skypiea.Helpers
+{
+    public class BlobHelper : IBlobHelper
+    {
+        private readonly CloudBlobClient _blobClient;
+        public BlobHelper(IConfiguration configuration)
+        {
+            string keys = configuration["Blob:ConnectionString"];
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(keys);
+            _blobClient = storageAccount.CreateCloudBlobClient();
+
+        }
+        public Task DeleteBlobAsync(Guid id, string containerName)
+        {
+            throw new NotImplementedException();
+        }
+e 4rre
+        public async Task<Guid> UploadBlobAsync(IFormFile file, string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Guid> UploadBlobAsync(byte[] file, string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Guid> UploadBlobAsync(string image, string containerName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
