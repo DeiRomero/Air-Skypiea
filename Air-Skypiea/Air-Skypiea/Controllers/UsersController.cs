8e9288dd1696682d35791de34eb4af9ce98b1750
DEI ROMERO <DEI ROMERO@DESKTOP-UNSED2L>
2022-05-28 23:50:02 -0500

    Initial Admin

---
+++ Air-Skypiea/Air-Skypiea/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Air_Skypiea.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Air_Skypiea.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Users
+                .Include(u => u.City)
+                .ThenInclude(c => c.State)
+                .ThenInclude(c => c.Country)
+                .ToListAsync());
+
+
+
+        }
+    }
+}
