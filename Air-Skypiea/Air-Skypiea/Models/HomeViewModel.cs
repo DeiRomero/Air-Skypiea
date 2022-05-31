13ab45e58b701630bc29b8a10cdfd85b784ad5ce
Jesaar <jesaar2000@gmail.com>
2022-05-30 22:14:44 -0500

    Footer y carousel

---
+++ Air-Skypiea/Air-Skypiea/Models/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Air_Skypiea.Data.Entities;
+
+namespace Air_Skypiea.Models
+{
+    public class HomeViewModel
+    {
+        //public PaginatedList<Flight> Flights { get; set; }
+
+
+        public ICollection<City> Cities { get; set; }
+        public float Quantity { get; set; }
+    }
+
+}
