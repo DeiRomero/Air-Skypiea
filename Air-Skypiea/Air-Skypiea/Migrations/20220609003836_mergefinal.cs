cab7646b4565745e39b5cc89f6731fb042830abc
Jesaar <jesaar2000@gmail.com>
2022-06-08 19:42:56 -0500

    fusion entre ramas y migracion mergefinal

---
+++ Air-Skypiea/Air-Skypiea/Migrations/20220609003836_mergefinal.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Air_Skypiea.Migrations
+{
+    public partial class mergefinal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
